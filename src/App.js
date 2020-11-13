2effa849395a60b978b8b90c1aa9bc52faf875d6
unknown <gabrielyan3g@gmail.com>
2020-11-13 19:40:47 +0400

    new animals

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ const App = () => {
     <div className='body__wrapper'>
       <Animal
         img='https://images.theconversation.com/files/290710/original/file-20190903-175663-lqb3z6.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop'
-        name='monkey' />
+        name='Monkey' />
       <Animal
-        img='https://images.theconversation.com/files/290710/original/file-20190903-175663-lqb3z6.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop'
-        name='monkey' />
+        img='https://i.pinimg.com/originals/c5/d6/6a/c5d66aa224f958d90a1c66f031fec857.jpg'
+        name='Wolf' />
       <Animal
-        img='https://images.theconversation.com/files/290710/original/file-20190903-175663-lqb3z6.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop'
-        name='monkey' />
+        img='https://images.wsj.net/im-196181?width=1280&size=1'
+        name='Tigre' />
     </div>
   );
 }
